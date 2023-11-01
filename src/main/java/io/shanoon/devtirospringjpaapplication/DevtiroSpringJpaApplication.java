d36ff52cc1d5bc83c0cfa7441b93febbf4729b0c
shanoonissaka <shanoon.issaka48@gmail.com>
2023-11-01 05:39:57 +0000

    Task: Basic configuration Complete: True

---
+++ src/main/java/io/shanoon/devtirospringjpaapplication/DevtiroSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package io.shanoon.devtirospringjpaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtiroSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtiroSpringJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/shanoon/devtirospringjpaapplication/DevtiroSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shanoon.devtirospringjpaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtiroSpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
