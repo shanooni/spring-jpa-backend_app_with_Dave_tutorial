9a555d949dbc9f022def27883b10e12ce1965a50
shanoonissaka <shanoon.issaka48@gmail.com>
2023-11-16 06:54:21 +0000

    Tasks: Nested Objects and cascading mapping between books and author configuration

--- src/main/java/io/shanoon/devtirospringjpaapplication/config/MapperConfig.java
+++ src/main/java/io/shanoon/devtirospringjpaapplication/config/MapperConfig.java
@@ -1,15 +1,18 @@
 package io.shanoon.devtirospringjpaapplication.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.management.modelmbean.ModelMBean;
 
 @Configuration
 public class MapperConfig {
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper
+                .getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
     }
 }
